f177e9c548717020639833b02743cb0cbf80c777
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2019-02-23 12:06:15 +0900

    Modify: Fixed not to make wifi connection at startup.

--- LovyanLauncher/src/CBFTPserverSPIFFS.h
+++ LovyanLauncher/src/CBFTPserverSPIFFS.h
@@ -11,6 +11,8 @@ public:
   FtpServerSPIFFS ftpSrv;
 
   bool setup(){
+    M5.Lcd.setTextFont(0);
+    me = this;
     M5.Lcd.setTextColor(0xFFFF);
     for (int i = 1; i < 16; ++i) {
       M5.Lcd.drawFastHLine(0, i, TFT_HEIGHT, i << 6);
@@ -18,13 +20,15 @@ public:
     M5.Lcd.drawString("FTP Server (SPIFFS)", 10, 0, 2);
 
     SPIFFS.begin();
-    ftpSrv.begin("M5","Stack");
-    M5.Lcd.setCursor(0,40);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println("host : " + WiFi.localIP().toString());
-    M5.Lcd.print("user : M5\r\npass : Stack\r\n\r\n");
-    M5.Lcd.setTextSize(1);
-    M5.Lcd.print("ftp://M5:Stack@" + WiFi.localIP().toString() + "/");
+    closing = false;
+    onevent = WiFi.onEvent(WiFiEvent);
+    if (WiFi.status() == WL_CONNECTED) {
+      startFTP();
+    } else {
+      WiFi.begin();
+      M5.Lcd.drawString("WiFi waiting...", 10, 60, 1);
+    }
+
     return true;
   }
 
@@ -35,6 +39,42 @@ public:
     return true;
   }
 
+  void close()
+  {
+    closing = true;
+    WiFi.removeEvent(onevent);
+    delay(100);
+  }
+
 private:
+  wifi_event_id_t onevent = 0;
+  static CBFTPserverSPIFFS* me;
+  static bool closing;
+  static void WiFiEvent(WiFiEvent_t event, system_event_info_t info){
+    if (closing) return;
+    switch(event){
+      case SYSTEM_EVENT_STA_GOT_IP:
+        me->startFTP();
+        break;
+      default:
+        break;
+    }
+  }
+
+  void startFTP()
+  {
+    ftpSrv.begin("M5","Stack");
+    M5.Lcd.setTextColor(0xFFFF, 0);
+    M5.Lcd.setCursor(0,60);
+    M5.Lcd.setTextFont(1);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.println("host : " + WiFi.localIP().toString());
+    M5.Lcd.print("user : M5\r\npass : Stack\r\n\r\n");
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setTextFont(2);
+    M5.Lcd.print("ftp://M5:Stack@" + WiFi.localIP().toString() + "/");
+  }
 };
+CBFTPserverSPIFFS* CBFTPserverSPIFFS::me;
+bool CBFTPserverSPIFFS::closing;
 #endif
